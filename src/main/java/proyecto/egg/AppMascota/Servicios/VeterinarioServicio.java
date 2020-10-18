becd1b1d67eff09c0d7071dc98f30101c240fc60
Pablo <pablo.scoccimarro@gmail.com>
2020-10-17 21:03:06 -0300

    creacion de consulta por id veterinario

--- src/main/java/proyecto/egg/AppMascota/Servicios/VeterinarioServicio.java
+++ src/main/java/proyecto/egg/AppMascota/Servicios/VeterinarioServicio.java
@@ -1,13 +1,14 @@
 package proyecto.egg.AppMascota.Servicios;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import proyecto.egg.AppMascota.Entidades.Veterinario;
 import proyecto.egg.AppMascota.Repositorios.VeterinarioRepositorio;
 
 public class VeterinarioServicio {
+
     @Autowired
     VeterinarioRepositorio veterinarioRepositorio;
-    
 
     public void registroVeterinario(String nombre, String matricula, String nombreClinica, String zona) {
         Veterinario veterinario = new Veterinario();
@@ -15,21 +16,34 @@ public class VeterinarioServicio {
         veterinario.setMatricula(matricula);
         veterinario.setNombreClinica(nombreClinica);
         veterinario.setZona(zona);
-        
+
+        veterinarioRepositorio.save(veterinario);
+
     }
-    
+
     public void bajaVeterinario(String matricula) {
-        
-    
-    
+
     }
-    
-    public void modificacionVeterinario(String nombre, String matricula, String nombreClinica, String zona){
-        
+
+    public void modificacionVeterinario(String nombre, String matricula, String nombreClinica, String zona) {
+
     }
-    
-    public void buscarVeterinario(String id){
+
+    public void buscarVeterinario(String matricula) {
         
+        Optional<Veterinario> v = veterinarioRepositorio.findById(matricula);
+        
+        if (v.isPresent()){
+            Veterinario veterinario = v.get();
+            veterinarioRepositorio.save(veterinario);
+            
+            
+        }
+        else{
+            System.out.println("No existe esa Matricula en nuestra base de datos. Por favor ingreselo.");
+        }
+        
+
     }
 
 }
