446bac0c48a5088fe29c021b8fff3db2727974bb
FedericoSegui <federsegui@gmail.com>
2020-10-26 11:01:54 -0300

    Actualizacion front consulta

---
+++ src/main/java/proyecto/egg/AppMascota/Controladores/MascotaControlador.java
@@ -0,0 +1,44 @@
+package proyecto.egg.AppMascota.Controladores;
+
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import proyecto.egg.AppMascota.Errores.ErrorServicio;
+import proyecto.egg.AppMascota.Servicios.MascotaServicio;
+
+
+
+@Controller
+@RequestMapping("/mascota")
+public class MascotaControlador {
+
+    @Autowired
+    private MascotaServicio mascotaServicio;
+
+    @GetMapping("")
+    public String mascota(){
+        return "crearMascota.html";
+    }
+
+    @PostMapping("/crearMascota")
+    public String crearMascota(ModelMap model, @RequestParam String nombre, @RequestParam String raza, @RequestParam Date fechaNacimiento) throws ErrorServicio {
+
+        try {
+            mascotaServicio.registroMascota(nombre, raza, fechaNacimiento);
+        } catch (ErrorServicio ex) {
+            model.put("error", ex.getMessage());
+            model.put("nombre", nombre);
+            model.put("raza", raza);
+            model.put("fechaNacimiento", fechaNacimiento);
+            return "crearMacota.html";
+        }
+        model.put("titulo", "Se creó la mascota");
+        return "exito";
+    }
+}
